590d9a4ddfb5899aaf73e7c6cd6d6104ea6d7eff
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-09-18 18:34:29 +0200

    Added EmployeeDAO Hibernate implementation

--- src/main/java/com/olejarzikt/springboot/crudemployee/dao/EmployeeDAO.java
+++ src/main/java/com/olejarzikt/springboot/crudemployee/dao/EmployeeDAO.java
@@ -1,2 +1,9 @@
-package com.olejarzikt.springboot.crudemployee.dao;public interface EmployeeDAO {
+package com.olejarzikt.springboot.crudemployee.dao;
+
+import com.olejarzikt.springboot.crudemployee.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> findAll();
 }
--- src/main/java/com/olejarzikt/springboot/crudemployee/dao/EmployeeDAOHibernateImpl.java
+++ src/main/java/com/olejarzikt/springboot/crudemployee/dao/EmployeeDAOHibernateImpl.java
@@ -1,2 +1,33 @@
-package com.olejarzikt.springboot.crudemployee.dao;public class EmployeeDAOHibernateImpl {
+package com.olejarzikt.springboot.crudemployee.dao;
+
+import com.olejarzikt.springboot.crudemployee.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Transient;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class EmployeeDAOHibernateImpl implements EmployeeDAO{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDAOHibernateImpl(EntityManager theEntityManager){
+        entityManager=theEntityManager;
+    }
+
+    @Override
+    @Transactional
+    public List<Employee> findAll() {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query<Employee> theQuery = currentSession.createQuery("from Employee",Employee.class);
+        List<Employee> employees = theQuery.getResultList();
+        return employees;
+    }
+
 }
