0c1cd6e0fc64ccdc5b1ae95b5b9d269652e56d86
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-09-18 16:51:37 +0200

    Initial commit

---
+++ src/main/java/com/olejarzikt/springboot/crudemployee/CrudemployeeApplication.java
@@ -0,0 +1,13 @@
+package com.olejarzikt.springboot.crudemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudemployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudemployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olejarzikt/springboot/crudemployee/CrudemployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olejarzikt.springboot.crudemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudemployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
