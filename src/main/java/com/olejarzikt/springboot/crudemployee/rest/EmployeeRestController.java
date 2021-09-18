2bdc096d3059a28db518d091fa780dbaab5beb6e
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-09-18 19:11:05 +0200

    Added Controller

--- src/main/java/com/olejarzikt/springboot/crudemployee/rest/EmployeeRestController.java
+++ src/main/java/com/olejarzikt/springboot/crudemployee/rest/EmployeeRestController.java
@@ -1,2 +1,27 @@
-package com.olejarzikt.springboot.crudemployee.rest;public class RestController {
+package com.olejarzikt.springboot.crudemployee.rest;
+
+import com.olejarzikt.springboot.crudemployee.dao.EmployeeDAO;
+import com.olejarzikt.springboot.crudemployee.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+        employeeDAO=theEmployeeDAO;
+    }
+
+    @GetMapping("/employess")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
 }
