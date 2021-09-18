786b6225151c4b6c4a1c7a886abe351b7e904cd5
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-09-18 18:11:30 +0200

    Created Employee entity

--- src/main/java/com/olejarzikt/springboot/crudemployee/entity/Employee.java
+++ src/main/java/com/olejarzikt/springboot/crudemployee/entity/Employee.java
@@ -1,2 +1,64 @@
-package com.olejarzikt.springboot.crudemployee.entity;public class Employee {
+package com.olejarzikt.springboot.crudemployee.entity;
+
+import javax.persistence.*;
+import javax.print.DocFlavor;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name  ")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String enail;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String enail) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.enail = enail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEnail() {
+        return enail;
+    }
+
+    public void setEnail(String enail) {
+        this.enail = enail;
+    }
 }
