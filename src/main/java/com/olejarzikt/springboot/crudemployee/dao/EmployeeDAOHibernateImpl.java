4ff8c0fce044cc91c4ab09c489c55297d3e62bc9
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-09-18 19:25:45 +0200

    Added rest of CRUD methods of DAO

--- src/main/java/com/olejarzikt/springboot/crudemployee/dao/EmployeeDAOHibernateImpl.java
+++ src/main/java/com/olejarzikt/springboot/crudemployee/dao/EmployeeDAOHibernateImpl.java
@@ -30,4 +30,26 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO{
         return employees;
     }
 
+    @Override
+    public Employee findById(int theId) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Employee theEmployee = currentSession.get(Employee.class,theId);
+        return theEmployee;
+    }
+
+    @Override
+    public void save(Employee theEmployee) {
+        Session currentSession = entityManager.unwrap(Session.class);
+         currentSession.saveOrUpdate(theEmployee);
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query theQuery = currentSession.createQuery("delete from Employee where id =:employeeId");
+        theQuery.setParameter("employeeId",theId);
+        theQuery.executeUpdate();
+    }
+
+
 }
